825310e89b67e0684309800fd21d1c818b0d6f4b
Amin Messaoudi <amin_messaoudi@yahoo.com>
2023-10-11 10:15:03 +0000

    TDD: Check that attempt exists in the English dictionary

--- longest_word/game.py
+++ longest_word/game.py
@@ -7,7 +7,7 @@ class Game:
     """
     def __init__(self) -> None:
         """Attribute a random grid to size 9"""
-        self.grid = random.choice(string.ascii_uppercase) for i in range(9)]
+        self.grid = [random.choice(string.ascii_uppercase) for i in range(9)]
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
